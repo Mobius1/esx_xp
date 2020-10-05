5cb4382420392bdbdbb497f451b9800d6dd689b6
Mobius1 <mobius1@gmx.com>
2020-10-05 12:54:14 +0100

    Fix translation error

--- locales/en.lua
+++ locales/en.lua
@@ -3,8 +3,8 @@ Locales["en"] = {
     ['cmd_current_lvl'] = "Your current rank is ^*^2 %s",
     ['cmd_next_lvl']    = "You require ^*^2 %s XP ^*^7to advance to rank ^*^2 %s",
 
-    ['err_xp_update']   = "esx_xp: Invalid rank (%s) passed to '%s'",
-    ['err_lvl_update']  = "esx_xp: Invalid rank (%s) passed to '%s'",
+    ['err_xp_update']   = "esx_xp: Invalid XP (%s) passed to '%s'",
+    ['err_lvl_update']  = "esx_xp: Invalid Rank (%s) passed to '%s'",
     ['err_lvls_check']  = "esx_xp: You have an error in %s ranks in '%s'",
     ['err_lvl_check']   = "Rank %s: %s",
 }
